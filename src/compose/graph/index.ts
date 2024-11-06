13faa7a2aac2a15e19e73f3b8c2b1f8b3da6bff8
Viktor Pasynok <v.pasynok@space307.com>
2024-11-07 00:14:40 +0300

    added: full coverage

--- src/compose/graph/index.ts
+++ src/compose/graph/index.ts
@@ -13,29 +13,6 @@ type Graph = Record<
   }
 >;
 
-/**
- * ### Rule for Classifying Dependencies
- *
- * 1. **Direct Dependencies**:
- *    - A dependency is considered **direct** if it is explicitly declared in the `dependsOn` or `optionalDependsOn` lists of a container.
- *    - Direct dependencies are categorized as:
- *      - **Strict**: If they are listed in `dependsOn`.
- *      - **Optional**: If they are listed in `optionalDependsOn`.
- *
- * 2. **Transitive Dependencies**:
- *    - A dependency is considered **transitive** if it is inherited through a chain of dependencies from another container, rather than being directly listed.
- *    - **Classification of Transitive Dependencies**:
- *      - If a dependency path begins from a **strict** direct dependency, all subsequent dependencies in the chain are also **strict**.
- *      - If a dependency path begins from an **optional** direct dependency, all subsequent dependencies in the chain are **optional**, regardless of their original type.
- *
- * 3. **Propagation of Dependency Type**:
- *    - The strictness of a transitive dependency is determined by the type of the initial direct dependency in the chain:
- *      - If the initial direct dependency is **strict**, the entire chain is considered **strict**.
- *      - If the initial direct dependency is **optional**, the entire chain is considered **optional**.
- *
- * This rule ensures that the classification of dependencies is consistent and reflects both direct and inherited relationships in a system.
- */
-
 const graphFn = (containers: AnyContainer[]) =>
   containers.reduce<Graph>((acc, container) => {
     const dependsOn = (container.dependsOn || []).map((x) => x.id);
