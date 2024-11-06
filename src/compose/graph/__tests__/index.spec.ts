6f6dcfb3e394dd48887a4103d015471b0d87a44f
Viktor Pasynok <v.pasynok@space307.com>
2024-11-06 23:48:21 +0300

    added: full coverage

---
+++ src/compose/graph/__tests__/index.spec.ts
@@ -0,0 +1,100 @@
+import { genContainerId } from '../../../__fixtures__';
+import { createContainer } from '../../../createContainer';
+import { graphFn } from '../index';
+
+const start = () => ({ api: null });
+
+test('handles all variations of dependencies', () => {
+  // containers without dependencies
+  const noDeps1 = createContainer({ id: genContainerId(), start });
+  const noDeps2 = createContainer({ id: genContainerId(), start });
+
+  // containers with strict dependencies
+  const strict1 = createContainer({ id: genContainerId(), dependsOn: [noDeps1], start });
+  const strict2 = createContainer({ id: genContainerId(), dependsOn: [strict1], start });
+
+  // containers with optional dependencies
+  const optional1 = createContainer({ id: genContainerId(), optionalDependsOn: [noDeps2], start });
+  const optional2 = createContainer({ id: genContainerId(), optionalDependsOn: [strict2], start });
+
+  // containers with mixed dependencies
+  const mixed = createContainer({
+    id: genContainerId(),
+    dependsOn: [strict1],
+    optionalDependsOn: [optional1],
+    start,
+  });
+
+  const graph = graphFn([noDeps1, noDeps2, strict1, strict2, optional1, optional2, mixed]);
+
+  expect(graph).toStrictEqual({
+    [noDeps1.id]: {
+      strict: [],
+      optional: [],
+      transitive: { strict: [], optional: [] },
+    },
+    [noDeps2.id]: {
+      strict: [],
+      optional: [],
+      transitive: { strict: [], optional: [] },
+    },
+    [strict1.id]: {
+      strict: [noDeps1.id],
+      optional: [],
+      transitive: { strict: [], optional: [] },
+    },
+    [strict2.id]: {
+      strict: [strict1.id],
+      optional: [],
+      transitive: {
+        strict: [
+          {
+            id: noDeps1.id,
+            path: `${strict2.id} -> ${strict1.id} -> ${noDeps1.id}`,
+          },
+        ],
+        optional: [],
+      },
+    },
+    [optional1.id]: {
+      strict: [],
+      optional: [noDeps2.id],
+      transitive: { strict: [], optional: [] },
+    },
+    [optional2.id]: {
+      strict: [],
+      optional: [strict2.id],
+      transitive: {
+        strict: [],
+        optional: [
+          {
+            id: strict1.id,
+            path: `${optional2.id} -> ${strict2.id} -> ${strict1.id}`,
+          },
+          {
+            id: noDeps1.id,
+            path: `${optional2.id} -> ${strict2.id} -> ${strict1.id} -> ${noDeps1.id}`,
+          },
+        ],
+      },
+    },
+    [mixed.id]: {
+      strict: [strict1.id],
+      optional: [optional1.id],
+      transitive: {
+        strict: [
+          {
+            id: noDeps1.id,
+            path: `${mixed.id} -> ${strict1.id} -> ${noDeps1.id}`,
+          },
+        ],
+        optional: [
+          {
+            id: noDeps2.id,
+            path: `${mixed.id} -> ${optional1.id} -> ${noDeps2.id}`,
+          },
+        ],
+      },
+    },
+  });
+});
