cf78e1cfd3bc4b3a02b0a05331019d385f57e23e
Viktor Pasynok <v.pasynok@space307.com>
2024-11-08 13:43:32 +0300

    removed: extra async

--- src/compose/up/index.ts
+++ src/compose/up/index.ts
@@ -59,7 +59,7 @@ const upFn = async <T extends AnyContainer[]>(containers: T, config?: { debug?: 
   let apis: Record<string, Awaited<ReturnType<AnyContainer['start']>>['api']> = {};
 
   await Promise.allSettled(
-    containers.map(async (container) => {
+    containers.map((container) => {
       const $strictDepsResolving: Store<ContainerStatus> = combine(
         (container.dependsOn ?? []).map((d) => d.$status),
         (x) => {
