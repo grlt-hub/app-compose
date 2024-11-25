7d00e2aea5bbce9a59f41ce768bbe1c8ef85fcf6
Viktor Pasynok <8722478+binjospookie@users.noreply.github.com>
2024-11-26 00:19:21 +0300

    28 (#30)
     added: analyze graph fns

---
+++ src/compose/graph/dependsOn.ts
@@ -0,0 +1,31 @@
+import type { AnyContainer } from '../../createContainer';
+import type { ContainerDomain, ContainersGraph, DomainsGraph } from './types';
+
+type PickResult<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+const pick = <T extends object, K extends keyof T>(obj: T, keys: K[]): PickResult<T, K> => {
+  const result = {} as PickResult<T, K>;
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+};
+
+const createDependsOn =
+  <T extends ContainersGraph | DomainsGraph>(graph: T) =>
+  (list: T extends ContainersGraph ? AnyContainer[] : ContainerDomain[]) => {
+    if (typeof list[0] === 'string') {
+      return pick(graph, list as ContainerDomain[]);
+    }
+
+    return pick(
+      graph,
+      (list as AnyContainer[]).map((item) => item.id),
+    );
+  };
+
+export { createDependsOn };
---
+++ src/compose/graph/requiredBy.ts
@@ -0,0 +1,37 @@
+import type { AnyContainer } from '../../createContainer';
+import type { ContainerDomain, ContainersGraph, DomainsGraph } from './types';
+
+const createRequiredBy =
+  <T extends ContainersGraph | DomainsGraph>(graph: T) =>
+  (list: T extends ContainersGraph ? AnyContainer[] : ContainerDomain[]) => {
+    const keys =
+      typeof list[0] === 'string' ? (list as ContainerDomain[]) : (list as AnyContainer[]).map((item) => item.id);
+
+    const entries = Object.entries(graph) as [keyof typeof graph, (typeof graph)[keyof typeof graph]][];
+
+    const result = {};
+
+    for (const [key, val] of entries) {
+      const strict = val.strict.filter((x) => keys.includes(x));
+      const optional = val.optional.filter((x) => keys.includes(x));
+      const transitiveStrict = val.transitive.strict.filter((x) => keys.includes(x.id));
+      const transitiveOptional = val.transitive.optional.filter((x) => keys.includes(x.id));
+
+      if (strict.length || optional.length || transitiveStrict.length || transitiveOptional.length) {
+        // @ts-expect-error :c
+        result[key] = {
+          ...('domain' in val ? { domain: val.domain } : { containers: val.containers }),
+          strict,
+          optional,
+          transitive: {
+            strict: transitiveStrict,
+            optional: transitiveOptional,
+          },
+        };
+      }
+    }
+
+    return result as T;
+  };
+
+export { createRequiredBy };
