2e8d10d5b49d798913894229f147845a8f064120
Viktor Pasynok <v.pasynok@space307.com>
2025-01-24 13:38:34 +0300

    added: debug test

---
+++ src/compose/commands/up/__tests__/debug.spec.ts
@@ -0,0 +1,21 @@
+import { createRandomContainer } from '@randomContainer';
+import { createStageUpFn } from '../createStageUpFn';
+
+test('up.debug = true', async () => {
+  const consoleLogSpy = vi.spyOn(console, 'debug').mockImplementation(() => {});
+
+  const a = createRandomContainer({ id: 'a' });
+  const b = createRandomContainer({ id: 'b', dependsOn: [a], enable: () => false });
+  const c = createRandomContainer({ id: 'c' });
+
+  const stage = {
+    id: '_',
+    containersToBoot: [a, b, c],
+  };
+
+  await createStageUpFn({ debug: true })(stage, {});
+
+  expect(consoleLogSpy.mock.calls).toMatchSnapshot();
+
+  consoleLogSpy.mockRestore();
+});
