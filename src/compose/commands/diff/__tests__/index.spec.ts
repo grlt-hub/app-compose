24ee1fd0e49f4a97cc8cd53224ac376f97cd4cc2
Viktor Pasynok <v.pasynok@space307.com>
2025-01-24 20:48:07 +0300

    added: test on diff

--- src/compose/commands/diff/__tests__/index.spec.ts
+++ src/compose/commands/diff/__tests__/index.spec.ts
@@ -90,4 +90,36 @@ describe('diff cmd', () => {
       ]
     `);
   });
+
+  test('with skipped', async () => {
+    const consoleLogSpy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+    const a = createRandomContainer();
+    const b = createRandomContainer({
+      dependencies: [a],
+    });
+    const c = createRandomContainer({
+      dependencies: [b],
+    });
+
+    diff([['x', [b, c]]], [{ id: 'x', containersToBoot: [a, b, c], skippedContainers: { [a.id]: ['skippedId'] } }]);
+
+    expect(consoleLogSpy.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          "[app-compose] | diff command
+      All skipped containers are optional. If they are expected to work, please include them in the list when calling \`compose\` function
+
+      Stages:",
+        ],
+        [
+          "- [35mx[39m:
+          input:  [ ${b.id}, ${c.id} ]
+          output: [ [42m${a.id}[49m, ${b.id}, ${c.id} ]
+          skipped:
+                  - [33mskippedId[39m: [${a.id}]",
+        ],
+      ]
+    `);
+  });
 });
