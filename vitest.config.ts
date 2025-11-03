d75d58fccecf0212ca3a26bf2aaedebc9db00da2
Viktor Pasynok <v.pasynok@space307.com>
2025-11-03 18:28:55 +0700

    chore: upd deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defaultExclude, defineConfig } from 'vitest/config';
 
+const cwd = process.cwd();
+
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     setupFiles: './vitest.setup.ts',
     environment: 'node',
@@ -17,9 +17,8 @@ export default defineConfig({
     globals: true,
     reporters: 'dot',
     coverage: {
-      extension: ['.ts'],
       all: true,
-      include: ['src/**/*'],
+      include: ['src/**/*.ts'],
       exclude: [...defaultExclude, '**/__fixtures__/**', '**/__tests__/**', './src/index.ts'],
       reporter: 'text',
       provider: 'v8',
@@ -30,6 +29,12 @@ export default defineConfig({
     css: false,
     watch: false,
     pool: 'threads',
+    alias: {
+      '@randomContainer': cwd + '/src/compose/__fixtures__/createRandomContainer',
+      '@createContainer': cwd + '/src/createContainer',
+      '@shared': cwd + '/src/shared',
+      '@prepareStages': cwd + '/src/compose/prepareStages',
+    },
   },
   resolve: {
     mainFields: ['module'],
