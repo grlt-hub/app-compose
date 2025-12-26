ad6b2ebbd632326a3522d8da421f095a63fda2e5
Viktor Pasynok <8722478+binjospookie@users.noreply.github.com>
2025-12-26 21:36:49 +0700

    removed: toDisplayName (#48)
    * removed: toDisplayName
    
    ---------
    
    Co-authored-by: Viktor Pasynok <v.pasynok@space307.com>

--- src/compose/toID.ts
+++ src/compose/toID.ts
@@ -2,21 +2,23 @@ import { Meta$ } from "@meta"
 import { LIBRARY_NAME } from "@shared"
 import { Binding$ } from "@tag"
 import { Task$ } from "@task"
-import type { Step, StepType } from "./types"
+import type { Step } from "./types"
 
 const toID = (step: Step) => {
   switch (true) {
     case Task$ in step:
       return {
-        type: "task" as StepType,
+        type: "task" as const,
         name: step[Meta$].name,
+        display: { name: `Task[${step[Meta$].name}]` },
         symbol: step[Task$].id.value,
         writes: [step[Task$].id.value, step[Task$].id.status],
       }
     case Binding$ in step:
       return {
-        type: "binding" as StepType,
+        type: "binding" as const,
         name: step[Meta$].name,
+        display: { name: `Tag[${step[Meta$].name}]` },
         symbol: step[Binding$].id,
         writes: [step[Binding$].id],
       }
