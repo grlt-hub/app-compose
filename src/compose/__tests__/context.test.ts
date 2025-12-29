35f51ecb9413d23e4eacfe712630a98857927162
Viktor Pasynok <v.pasynok@space307.com>
2025-12-29 22:31:26 +0700

    removed: _fn

--- src/compose/__tests__/context.test.ts
+++ src/compose/__tests__/context.test.ts
@@ -9,156 +9,156 @@ const alphaTask = createTask({ name: "alpha", run: { fn: () => ({ api: "alpha_ap
 
 describe("task context resolution", () => {
   it("void contexts | run.fn", async () => {
-    const run_fn = vi.fn()
+    const run = vi.fn()
 
-    const betaTask = createTask({ name: "beta", run: { fn: run_fn } })
+    const betaTask = createTask({ name: "beta", run: { fn: run } })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
+    expect(run).toBeCalledWith(undefined)
   })
 
   it("void contexts | both", async () => {
-    const run_fn = vi.fn()
-    const enabled_fn = vi.fn(() => true)
+    const run = vi.fn()
+    const enabled = vi.fn(() => true)
 
-    const betaTask = createTask({ name: "beta", run: { fn: run_fn }, enabled: { fn: enabled_fn } })
+    const betaTask = createTask({ name: "beta", run: { fn: run }, enabled: { fn: enabled } })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
-    expect(enabled_fn).toBeCalledWith(undefined)
+    expect(run).toBeCalledWith(undefined)
+    expect(enabled).toBeCalledWith(undefined)
   })
 
   it("simple contexts | run.fn", async () => {
-    const run_fn = vi.fn<(__: string) => void>()
+    const run = vi.fn<(__: string) => void>()
 
-    const betaTask = createTask({ name: "beta", run: { fn: run_fn, context: alphaTask.api } })
+    const betaTask = createTask({ name: "beta", run: { fn: run, context: alphaTask.api } })
 
     const scope = await compose().stage([alphaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith(scope.get(alphaTask)?.api)
+    expect(run).toBeCalledWith(scope.get(alphaTask)?.api)
   })
 
   it("simple contexts | enabled.fn", async () => {
-    const run_fn = vi.fn()
-    const enabled_fn = vi.fn((__: string) => true)
+    const run = vi.fn()
+    const enabled = vi.fn((__: string) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn },
-      enabled: { fn: enabled_fn, context: alphaTask.api },
+      run: { fn: run },
+      enabled: { fn: enabled, context: alphaTask.api },
     })
 
     const scope = await compose().stage([alphaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
-    expect(enabled_fn).toBeCalledWith(scope.get(alphaTask)?.api)
+    expect(run).toBeCalledWith(undefined)
+    expect(enabled).toBeCalledWith(scope.get(alphaTask)?.api)
   })
 
   it("simple contexts | both", async () => {
-    const run_fn = vi.fn<(__: string) => void>()
-    const enabled_fn = vi.fn((__: string) => true)
+    const run = vi.fn<(__: string) => void>()
+    const enabled = vi.fn((__: string) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: alphaTask.api },
-      enabled: { fn: enabled_fn, context: alphaTask.api },
+      run: { fn: run, context: alphaTask.api },
+      enabled: { fn: enabled, context: alphaTask.api },
     })
 
     const scope = await compose().stage([alphaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith(scope.get(alphaTask)?.api)
-    expect(enabled_fn).toBeCalledWith(scope.get(alphaTask)?.api)
+    expect(run).toBeCalledWith(scope.get(alphaTask)?.api)
+    expect(enabled).toBeCalledWith(scope.get(alphaTask)?.api)
   })
 
   it("contexts are isolated", async () => {
-    const run_fn = vi.fn<(__: { x: string }) => void>()
-    const enabled_fn = vi.fn((__: { x: number }) => true)
+    const run = vi.fn<(__: { x: string }) => void>()
+    const enabled = vi.fn((__: { x: number }) => true)
 
     const stringTag = createTag<string>({ name: "string" })
     const numberTag = createTag<number>({ name: "number" })
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: { x: stringTag } },
-      enabled: { fn: enabled_fn, context: { x: numberTag } },
+      run: { fn: run, context: { x: stringTag } },
+      enabled: { fn: enabled, context: { x: numberTag } },
     })
 
     const stages: [Stage, Stage] = [[bind(stringTag, literal("str")), bind(numberTag, literal(7))], [betaTask]]
     await compose()
       .stage(...stages)
       .run()
-    expect(run_fn).toBeCalledWith({ x: "str" })
-    expect(enabled_fn).toBeCalledWith({ x: 7 })
+    expect(run).toBeCalledWith({ x: "str" })
+    expect(enabled).toBeCalledWith({ x: 7 })
   })
 
   it("literal | run.fn", async () => {
-    const run_fn = vi.fn<(__: string) => void>()
+    const run = vi.fn<(__: string) => void>()
 
-    const betaTask = createTask({ name: "beta", run: { fn: run_fn, context: literal("str") } })
+    const betaTask = createTask({ name: "beta", run: { fn: run, context: literal("str") } })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith("str")
+    expect(run).toBeCalledWith("str")
   })
 
   it("literal | enabled.fn", async () => {
-    const run_fn = vi.fn()
-    const enabled_fn = vi.fn((__: string) => true)
+    const run = vi.fn()
+    const enabled = vi.fn((__: string) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn },
-      enabled: { fn: enabled_fn, context: literal("str") },
+      run: { fn: run },
+      enabled: { fn: enabled, context: literal("str") },
     })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
-    expect(enabled_fn).toBeCalledWith("str")
+    expect(run).toBeCalledWith(undefined)
+    expect(enabled).toBeCalledWith("str")
   })
 
   it("literal | both", async () => {
-    const run_fn = vi.fn<(__: number) => void>()
-    const enabled_fn = vi.fn((__: string) => true)
+    const run = vi.fn<(__: number) => void>()
+    const enabled = vi.fn((__: string) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: literal(7) },
-      enabled: { fn: enabled_fn, context: literal("str") },
+      run: { fn: run, context: literal(7) },
+      enabled: { fn: enabled, context: literal("str") },
     })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(7)
-    expect(enabled_fn).toBeCalledWith("str")
+    expect(run).toBeCalledWith(7)
+    expect(enabled).toBeCalledWith("str")
   })
 
   it("status | run.fn", async () => {
-    const run_fn = vi.fn<(__: TaskStatus) => void>()
+    const run = vi.fn<(__: TaskStatus) => void>()
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: status(alphaTask) },
+      run: { fn: run, context: status(alphaTask) },
     })
 
     await compose().stage([alphaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith({ name: "done" })
+    expect(run).toBeCalledWith({ name: "done" })
   })
 
   it("status | enabled.fn", async () => {
-    const run_fn = vi.fn()
-    const enabled_fn = vi.fn((__: TaskStatus) => true)
+    const run = vi.fn()
+    const enabled = vi.fn((__: TaskStatus) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn },
-      enabled: { fn: enabled_fn, context: status(alphaTask) },
+      run: { fn: run },
+      enabled: { fn: enabled, context: status(alphaTask) },
     })
 
     await compose().stage([alphaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
-    expect(enabled_fn).toBeCalledWith({ name: "done" })
+    expect(run).toBeCalledWith(undefined)
+    expect(enabled).toBeCalledWith({ name: "done" })
   })
 
   it("status | both", async () => {
     const warn = vi.spyOn(console, "warn").mockImplementation(() => {})
 
-    const run_fn = vi.fn<(__: TaskStatus) => void>()
-    const enabled_fn = vi.fn((__: TaskStatus) => true)
+    const run = vi.fn<(__: TaskStatus) => void>()
+    const enabled = vi.fn((__: TaskStatus) => true)
 
     const gammaTask = createTask({
       name: "gamma",
@@ -171,41 +171,41 @@ describe("task context resolution", () => {
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: status(gammaTask) },
-      enabled: { fn: enabled_fn, context: status(alphaTask) },
+      run: { fn: run, context: status(gammaTask) },
+      enabled: { fn: enabled, context: status(alphaTask) },
     })
 
     await compose().stage([alphaTask], [gammaTask], [betaTask]).run()
-    expect(run_fn).toBeCalledWith({ name: "fail", error: Error("bip-bop") })
-    expect(enabled_fn).toBeCalledWith({ name: "done" })
+    expect(run).toBeCalledWith({ name: "fail", error: Error("bip-bop") })
+    expect(enabled).toBeCalledWith({ name: "done" })
 
     warn.mockRestore()
   })
 
   it("optional | run.fn", async () => {
-    const run_fn = vi.fn<(__?: string) => void>()
+    const run = vi.fn<(__?: string) => void>()
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn, context: optional(alphaTask.api) },
+      run: { fn: run, context: optional(alphaTask.api) },
     })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
+    expect(run).toBeCalledWith(undefined)
   })
 
   it("optional | enabled.fn", async () => {
-    const run_fn = vi.fn()
-    const enabled_fn = vi.fn((__?: string) => true)
+    const run = vi.fn()
+    const enabled = vi.fn((__?: string) => true)
 
     const betaTask = createTask({
       name: "beta",
-      run: { fn: run_fn },
-      enabled: { fn: enabled_fn, context: optional(alphaTask.api) },
+      run: { fn: run },
+      enabled: { fn: enabled, context: optional(alphaTask.api) },
     })
 
     await compose().stage([betaTask]).run()
-    expect(run_fn).toBeCalledWith(undefined)
-    expect(run_fn).toBeCalledWith(undefined)
+    expect(run).toBeCalledWith(undefined)
+    expect(run).toBeCalledWith(undefined)
   })
 })
