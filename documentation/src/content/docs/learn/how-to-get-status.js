abeb4e445cccaa4624a9f5a8dd920af92ee5c335
Viktor Pasynok <v.pasynok@space307.com>
2026-01-09 18:23:50 +0700

    added: How to get Task status

--- documentation/src/content/docs/learn/how-to-get-status.js
+++ documentation/src/content/docs/learn/how-to-get-status.js
@@ -1,33 +1,41 @@
-import { bind, compose, createTag, status } from "@grlt-hub/app-compose"
-
-const tag = createTag({ name: "userId" })
-
-const fetchUser = createTask({
-  name: "fetch-user",
-  run: {
-    fn: async (ctx) => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${ctx.userId}`)
-      const result = await response.json()
-
-      console.log(JSON.stringify(result, null, 2))
-    },
-    context: { userId: tag },
-  },
-})
+import { status } from "@grlt-hub/app-compose"
 
 const logIn = createTask({
   name: "log-in",
   run: {
-    fn: () => ({ id: 1 }),
+    fn: () => {
+      // 👇 Uncomment to simulate failure
+      // throw new Error("[log-in]: failed")
+    },
+  },
+})
+
+const fetchUser = createTask({
+  name: "fetch-user",
+  run: {
+    fn: () => {
+      // 👇 Uncomment to simulate failure
+      // throw new Error("[fetch-user]: failed")
+    },
   },
 })
 
 const controlTask = createTask({
-  name: "control-task",
+  name: "control",
+  run: {
+    fn: ({ checks }) => {
+      const failure = checks.some(Boolean)
+
+      if (failure) {
+        console.log("Something went wrong. Please try again.")
+      } else {
+        console.log("Everything is working!")
+      }
+    },
+    context: {
+      checks: [status(logIn, "fail"), status(fetchUser, "fail")],
+    },
+  },
 })
 
-compose()
-  .stage([logIn])
-  .stage([bind(tag, logIn.id)])
-  .stage([fetchUser])
-  .run()
+compose().stage([logIn], [fetchUser]).stage([controlTask]).run()
