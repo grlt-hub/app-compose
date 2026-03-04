e4618c60c4b7000ae55ebc198f2595f7aa45450b
Viktor Pasynok <v.pasynok@space307.com>
2026-03-04 15:29:01 +0700

    added: export StageConfig

--- packages/app-compose/src/compose/index.ts
+++ packages/app-compose/src/compose/index.ts
@@ -1,2 +1,3 @@
 export { compose } from "./compose"
 export type { Logger as ComposeLogger } from "./logger"
+export type { StageConfig } from "./definition"
--- packages/app-compose/src/index.ts
+++ packages/app-compose/src/index.ts
@@ -1,3 +1,3 @@
-export { compose, type ComposeLogger } from "@compose"
+export { compose, type ComposeLogger, type StageConfig } from "@compose"
 export { literal, map, optional, type SpotValue } from "@computable"
 export { bind, createTag, createTask, type Task, type TaskResult, type TaskStatus } from "@runnable"
