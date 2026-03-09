e6658599f8f94ca68003aa0125292bca2a505c09
Viktor Pasynok <v.pasynok@space307.com>
2026-03-09 22:03:58 +0700

    changed: home and quick-start

--- documentation/src/content/docs/learn/typescript/logger-type.ts
+++ documentation/src/content/docs/learn/typescript/logger-type.ts
@@ -1,7 +1,7 @@
 import { type ComposeLogger } from "@grlt-hub/app-compose"
 
 const myLogger: ComposeLogger = {
-  onTaskFail: (event) => event,
-  onStageStart: (event) => event,
-  onStageComplete: (event) => event,
+  onTaskFail: (event) => console.log(event),
+  onStageStart: (event) => console.log(event),
+  onStageComplete: (event) => console.log(event),
 }
