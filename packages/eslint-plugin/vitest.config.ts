d59a4142cb44e9af9429cc2f017bec676ec4ae77
Viktor Pasynok <v.pasynok@space307.com>
2026-05-21 14:10:13 +0700

    removed: coverage dep

--- packages/eslint-plugin/vitest.config.ts
+++ packages/eslint-plugin/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     setupFiles: ["src/setup.ts"],
-    coverage: { reporter: ["text"] },
   },
 
   resolve: { alias: { "@": "./src" } },
